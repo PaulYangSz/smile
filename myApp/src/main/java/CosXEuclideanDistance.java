a4ddc7576054af09f8d5ad72b57874966933869f
PaulYangSz <shohokuooo@gmail.com>
2017-05-23 16:31:47 +0800

    Complete the CosXEuclideanDistance.

--- myApp/src/main/java/CosXEuclideanDistance.java
+++ myApp/src/main/java/CosXEuclideanDistance.java
@@ -14,7 +14,7 @@
  * limitations under the License.
  *******************************************************************************/
 
-package smile.math.distance;
+import smile.math.distance.Metric;
 
 import java.io.Serializable;
 
@@ -26,8 +26,10 @@ import java.io.Serializable;
  * to save space.
  *
  * @author Haifeng Li
+ *
+ * We use the cos(latitude) in Euclidean distance: d = ( (x1-x2)^2 + (y1-y2)*cos((x1+x2)/2) ^ 2 )^(1/2)
  */
-public class EuclideanDistance implements Metric<double[]>, Serializable {
+public class CosXEuclideanDistance implements Metric<double[]>, Serializable {
     private static final long serialVersionUID = 1L;
 
     /**
@@ -38,15 +40,15 @@ public class EuclideanDistance implements Metric<double[]>, Serializable {
     /**
      * Constructor. Standard (unweighted) Euclidean distance.
      */
-    public EuclideanDistance() {
+    public CosXEuclideanDistance() {
     }
 
     /**
      * Constructor with a given weight vector.
-     * 
+     *
      * @param weight the weight vector.
      */
-    public EuclideanDistance(double[] weight) {
+    public CosXEuclideanDistance(double[] weight) {
         for (int i = 0; i < weight.length; i++) {
             if (weight[i] < 0)
                 throw new IllegalArgumentException(String.format("Weight has to be nonnegative: %f", weight[i]));
@@ -70,12 +72,20 @@ public class EuclideanDistance implements Metric<double[]>, Serializable {
     public double d(int[] x, int[] y) {
         if (x.length != y.length)
             throw new IllegalArgumentException(String.format("Arrays have different length: x[%d], y[%d]", x.length, y.length));
+        if (x.length != 2)
+            throw new IllegalArgumentException(String.format("Arrays length must be 2---(lat, lng)") );
 
         double dist = 0.0;
 
         if (weight == null) {
             for (int i = 0; i < x.length; i++) {
-                double d = x[i] - y[i];
+                double d = 0;
+                if(i == 0) { /* LAT */
+                    d = x[i] - y[i];
+                }
+                else { /* LNG */
+                    d = Math.cos( (double)( (x[0]+y[0])/2 ) ) * (x[i] - y[i]);
+                }
                 dist += d * d;
             }
         } else {
@@ -83,7 +93,13 @@ public class EuclideanDistance implements Metric<double[]>, Serializable {
                 throw new IllegalArgumentException(String.format("Input vectors and weight vector have different length: %d, %d", x.length, weight.length));
 
             for (int i = 0; i < x.length; i++) {
-                double d = x[i] - y[i];
+                double d = 0;
+                if(i == 0) { /* LAT */
+                    d = x[i] - y[i];
+                }
+                else { /* LNG */
+                    d = Math.cos( (double)( (x[0]+y[0])/2 ) ) * (x[i] - y[i]);
+                }
                 dist += weight[i] * d * d;
             }
         }
@@ -101,6 +117,8 @@ public class EuclideanDistance implements Metric<double[]>, Serializable {
     public double d(float[] x, float[] y) {
         if (x.length != y.length)
             throw new IllegalArgumentException(String.format("Arrays have different length: x[%d], y[%d]", x.length, y.length));
+        if (x.length != 2)
+            throw new IllegalArgumentException(String.format("Arrays length must be 2---(lat, lng)") );
 
         int n = x.length;
         int m = 0;
@@ -110,7 +128,13 @@ public class EuclideanDistance implements Metric<double[]>, Serializable {
             for (int i = 0; i < n; i++) {
                 if (!Float.isNaN(x[i]) && !Float.isNaN(y[i])) {
                     m++;
-                    double d = x[i] - y[i];
+                    double d = 0;
+                    if(i == 0) { /* LAT */
+                        d = x[i] - y[i];
+                    }
+                    else { /* LNG */
+                        d = Math.cos( (double)( (x[0]+y[0])/2 ) ) * (x[i] - y[i]);
+                    }
                     dist += d * d;
                 }
             }
@@ -121,7 +145,13 @@ public class EuclideanDistance implements Metric<double[]>, Serializable {
             for (int i = 0; i < n; i++) {
                 if (!Float.isNaN(x[i]) && !Float.isNaN(y[i])) {
                     m++;
-                    double d = x[i] - y[i];
+                    double d = 0;
+                    if(i == 0) { /* LAT */
+                        d = x[i] - y[i];
+                    }
+                    else { /* LNG */
+                        d = Math.cos( (double)( (x[0]+y[0])/2 ) ) * (x[i] - y[i]);
+                    }
                     dist += weight[i] * d * d;
                 }
             }
@@ -147,6 +177,8 @@ public class EuclideanDistance implements Metric<double[]>, Serializable {
     public double d(double[] x, double[] y) {
         if (x.length != y.length)
             throw new IllegalArgumentException(String.format("Arrays have different length: x[%d], y[%d]", x.length, y.length));
+        if (x.length != 2)
+            throw new IllegalArgumentException(String.format("Arrays length must be 2---(lat, lng)") );
 
         int n = x.length;
         int m = 0;
@@ -156,7 +188,13 @@ public class EuclideanDistance implements Metric<double[]>, Serializable {
             for (int i = 0; i < n; i++) {
                 if (!Double.isNaN(x[i]) && !Double.isNaN(y[i])) {
                     m++;
-                    double d = x[i] - y[i];
+                    double d = 0;
+                    if(i == 0) { /* LAT */
+                        d = x[i] - y[i];
+                    }
+                    else { /* LNG */
+                        d = Math.cos( (double)( (x[0]+y[0])/2 ) ) * (x[i] - y[i]);
+                    }
                     dist += d * d;
                 }
             }
@@ -167,7 +205,13 @@ public class EuclideanDistance implements Metric<double[]>, Serializable {
             for (int i = 0; i < n; i++) {
                 if (!Double.isNaN(x[i]) && !Double.isNaN(y[i])) {
                     m++;
-                    double d = x[i] - y[i];
+                    double d = 0;
+                    if(i == 0) { /* LAT */
+                        d = x[i] - y[i];
+                    }
+                    else { /* LNG */
+                        d = Math.cos( (double)( (x[0]+y[0])/2 ) ) * (x[i] - y[i]);
+                    }
                     dist += weight[i] * d * d;
                 }
             }
